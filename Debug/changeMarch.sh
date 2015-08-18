59a0b6cff0149c4e284d4c121137b053a1b52ba0
Romain Guyard <romain.guyard@parrot.com>
2015-08-18 17:59:25 +0200

    changeMArch now working in the folder third too

--- Debug/changeMarch.sh
+++ Debug/changeMarch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 find ./src/ -type f -print0 | xargs -0 sed -i 's/-march=native/-march=armv6zk -mcpu=arm1176jzf-s -mfloat-abi=hard -mfpu=vfp/g'
+find ./third/ -type f -print0 | xargs -0 sed -i 's/-march=native/-march=armv6zk -mcpu=arm1176jzf-s -mfloat-abi=hard -mfpu=vfp/g'
 find ./src/ -type f -print0 | xargs -0 sed -i 's/-g3//g'
+find ./third/ -type f -print0 | xargs -0 sed -i 's/-g3//g'
